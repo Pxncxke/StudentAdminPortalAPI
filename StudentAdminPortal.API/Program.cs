d394e3ff94ab73e6d7dcbf2669f23a1e3063427c
Erick Aulestia <pxncxke@gmail.com>
2022-09-01 23:58:31 -0500

    added controller, domain models

--- StudentAdminPortal.API/Program.cs
+++ StudentAdminPortal.API/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<StudentAdminContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("StudentAdminPortalDb")));
+
+builder.Services.AddScoped<IStudentRepository, SqlStudentRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "StudentAdminPortal.API", Version = "v1"}));
+
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
